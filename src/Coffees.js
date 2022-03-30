3e516fe4862d05f44c2aaed67ad74fcaa79a857d
Megan Martel <mmartel1330@gmail.com>
2022-03-30 13:17:22 -0400

    clode cleanup

--- src/Coffees.js
+++ src/Coffees.js
@@ -1,25 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function Coffees () {
-    const [coffeeList, setCoffeeList] = useState();
-    useEffect(() => {
-        fetch('https://api.sampleapis.com/coffee/hot')
-        .then(response => response.json())
-        .then(data => setCoffeeList(data)) //when we get list of 22 coffess were gna set it equal to setCoffeeList (so our coffee list will be that array)
-        .catch(err => console.error(err))
-    }, [])
-    return (
-        <>
-        <h2>Coffee Recipes</h2>
-        {!coffeeList
-            ? <h3>Loading...</h3>
-        
-        : <ol>{coffeeList.map(coffee => {
-            return <li key ={coffee.id}>{coffee.title}</li>
-        })}</ol>
-    }
-        </>
-        )
-    }
+function Coffees() {
+  const [coffeeList, setCoffeeList] = useState();
+  const [temperature, setTemperature] = useState("hot");
+  useEffect(() => {
+    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+      .then((response) => response.json())
+      .then((data) => setCoffeeList(data)) 
+      .catch((err) => console.error(err));
+  }, [temperature]);
+  return (
+    <>
+      <h2>Coffee Recipes</h2>
+      <button onClick={() => setTemperature("hot")}>Hot</button>
+      <button onClick={() => setTemperature("iced")}>Iced</button>
+      {!coffeeList ? (
+        <h3>Loading...</h3>
+      ) : (
+        <ol>
+          {coffeeList.map((coffee) => {
+            return <li key={coffee.id}>{coffee.title}</li>;
+          })}
+        </ol>
+      )}
+    </>
+  );
+}
 
 export default Coffees;
